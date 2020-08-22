6f522052b5aec005d1b329f47d63df41e09e131a
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-08-22 08:26:48 +0600

    Cleaned while revising

--- Algorithms/Sieve/Sieve Vs General Time comparism.cpp
+++ Algorithms/Sieve/Sieve Vs General Time comparism.cpp
@@ -1,26 +1,11 @@
-//arafat islam, 1507017, cse, KUET.
-//aiproshar
 //sieve of Eratosthenes VS Normal method
 /*
    Compare between Time difference Between sieve and general method of finding prime
     for sieve memory complexity is O(N) and time complexity is O(log(logN))
     for normal method (little bit optimized) tome complexity is O(N^2) ans memory complexity is 1.
 */
-#define LL                  long long
-#define UL                  unsigned long long
-#define PI                  acos(-1.0)
-#define Di(x)               int x;scanf("%d",&x)
-#define Si(x)               scanf("%d",&x);
-#define So(x)               printf("%d",x)
-#define pb                  push_back
-#define mp                  make_pair
-#define mset(name,value)    memset(name,value,sizeof(name))
-#define IO                  ios_base::sync_with_stdio(0);cin.tie(0)
-#define READ                freopen("C:/Users/arafat_islam/Desktop/in.txt","r",stdin)
-#define WRITE               freopen("C:/Users/arafat_islam/Desktop/out.txt","w",stdout)
 //----------------------------------------------------------------------------------------
 #include<bits/stdc++.h>
-#include<conio.h>
 using namespace std;
 vector<int>v1; // We can also declare an static array here
 //array must be declared globally so that sieve can access it
@@ -34,7 +19,7 @@ void sieve(void)
                 v1[j*i] = 0;
     }
 }
-bool isprime(LL x)
+bool isprime(long long int x)
 {
     if(x == 1)
         return false;
@@ -59,13 +44,17 @@ int main()
     //setting the memory to one. Indexing starts from 0 so size+1 is used
     //for static array use memset :)
     for(int i = 1 ; i <= v1.size() ; i++)
-        if(v1[i]);
-            //cout<<i<<endl;//if you wish you can comment out the this statement , it will print the primes
+        if(v1[i])
+        {
+            cout<<i<<endl; //comment out output to measure raw calculation time
+        }
     cout<<"Time taken(ms) in sieve() : "<<clock() - time<<endl;//time taken after giving the input
     time = clock();
     for(int i = 1 ; i <= v1.size()-1 ; i++)
-        if(isprime(i));
-            //cout<<i<<endl;//if you wish you can comment out the this statement , it will print the primes
+        if(isprime(i))
+        {
+            cout<<i<<endl; //comment out output to measure raw calculation time
+        }
     cout<<"Time taken(ms) in isprime() : "<<clock() - time<<endl;
 
     return 0;
--- Algorithms/Sieve/Sieve.cpp
+++ Algorithms/Sieve/Sieve.cpp
@@ -1,4 +1,3 @@
-//arafat islam, 1507017, cse, KUET.
 //aiproshar
 //sieve of Eratosthenes :)
 /*
@@ -13,21 +12,9 @@
     have started from 2*that number but its not needed because the multiples of numbers smaller than square of that
     number was marked zero by the smaller prime numbers :)) . Like (10 , 15, 20 were marked zero by 2, 3 ,2 respectively)
 */
-#define LL                  long long
-#define UL                  unsigned long long
-#define PI                  acos(-1.0)
-#define Di(x)               int x;scanf("%d",&x)
-#define Si(x)               scanf("%d",&x);
-#define So(x)               printf("%d",x)
-#define pb                  push_back
-#define mp                  make_pair
-#define mset(name,value)    memset(name,value,sizeof(name))
-#define IO                  ios_base::sync_with_stdio(0);cin.tie(0)
-#define READ                freopen("C:/Users/arafat_islam/Desktop/in.txt","r",stdin)
-#define WRITE               freopen("C:/Users/arafat_islam/Desktop/out.txt","w",stdout)
+
 //----------------------------------------------------------------------------------------
 #include<bits/stdc++.h>
-#include<conio.h>
 using namespace std;
 vector<int>v1; // We can also declare an static array here
 //array must be declared globally so that sieve can access it
