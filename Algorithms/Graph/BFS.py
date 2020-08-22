0f9e2c873c2239d1b4588a0382e6402e1dc3aee8
arafat islam <24490400+aiproshar@users.noreply.github.com>
2020-08-22 07:56:58 +0600

    Update BFS.py
    Bug Fix
    Specifying type of callable

--- Algorithms/Graph/BFS.py
+++ Algorithms/Graph/BFS.py
@@ -8,25 +8,25 @@ class Graph:
     self.E[u].append(v)
     self.E[v].append(u)
 
-def BFS(source, Graph, cost = 1):
-  not_visited = defaultdict(lambda:True)
+def BFS(source: int, G: Graph, cost: int = 1) -> (defaultdict, defaultdict):
+  visited = defaultdict(lambda:False)
   parent = defaultdict(lambda:-1)
   depth = defaultdict(lambda:-1)
 
   Q = deque()
   Q.append(source)
-  not_visited[source] = False
+  visited[source] = True
   depth[source] = 0
   while(len(Q)):
     Vertex_Current = Q.popleft()
     for connected in G.E[Vertex_Current]:
-      if(not_visited[connected]):
-        not_visited[connected] = False
+      if(not visited[connected]):
+        visited[connected] = True
         parent[connected] = Vertex_Current
         depth[connected] = depth[Vertex_Current]+cost
         Q.append(connected)
       
-  return depth
+  return depth, parent
 
 G = Graph(6)
 
@@ -37,8 +37,8 @@ G.insert_edge(2,3)
 G.insert_edge(3,5)
 G.insert_edge(4,5)
 
-depth = BFS(5,G)
+depth, parent = BFS(4,G)
 
 for i in range(6):
-  print(depth[i])
+  print("Node: "+ str(i) + " Depth : " + str(depth[i]) + " Parent " + str(parent[i]))
 
